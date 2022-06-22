9ec17bb644bb3bc4fc9ff72a59ded8701e257b09
mujibgithub <mujibulhaqm1@gmail.com>
2022-06-22 14:39:54 -0700

    commit third class

---
+++ src/day/one/thirdClassAddedPacak1.java
@@ -0,0 +1,10 @@
+package day.one;
+
+public class thirdClassAddedPacak1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("pacakGE 1 CLASS 3 ADDED");
+	}
+
+}
---
+++ src/day/two/ThirdClassAdded.java
@@ -0,0 +1,10 @@
+package day.two;
+
+public class ThirdClassAdded {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
